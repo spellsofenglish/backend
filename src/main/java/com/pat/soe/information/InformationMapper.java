d88576bffecd12f76b7bf8a5ba40ca86de2b73d3
UladzislauM <marshal25@yandex.by>
2023-03-03 10:35:01 +0300

    SOE_US5_T1: Create Mapper for Information.java

---
+++ src/main/java/com/pat/soe/information/InformationMapper.java
@@ -0,0 +1,10 @@
+package com.pat.soe.information;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface InformationMapper {
+    InformationDto informationToInformationDto (Information information);
+    Information informationDtoToInformation (InformationDto informationDto);
+    Information informationDtoForSaveToInformation (InformationDtoForSave dtoForSave);
+}
