2dccf6071f000233d9c56d002f9d3f560e9e4000
UladzislauM <marshal25@yandex.by>
2023-06-13 09:22:18 +0300

    Task for Refactor Registration: changed mapper

---
+++ src/main/java/com/pat/soe/mapper/user/UserMapper.java
@@ -0,0 +1,60 @@
+package com.pat.soe.mapper.user;
+
+import com.pat.soe.dto.user.UserDto;
+import com.pat.soe.dto.user.UserDtoForResponse;
+import com.pat.soe.dto.user.UserDtoForSave;
+import com.pat.soe.dto.user.UserDtoForUpdate;
+import com.pat.soe.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public User userDtoForSaveToUser(UserDtoForSave userDtoForSave) {
+        User user = new User();
+        user.setEmail(userDtoForSave.email());
+        user.setPassword(userDtoForSave.password());
+        user.setNickName(userDtoForSave.nickName());
+        return user;
+    }
+
+    public UserDto userToUserDto(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getNickName(),
+                user.getResult(),
+                user.getRole(),
+                user.isActive()
+        );
+    }
+
+    public User userDtoForResponseToUser(UserDtoForResponse userDtoForResponse) {
+        User user = new User();
+        user.setId(userDtoForResponse.id());
+        user.setEmail(userDtoForResponse.email());
+        user.setNickName(userDtoForResponse.nickName());
+        user.setRole(userDtoForResponse.role());
+        return user;
+    }
+
+    public User userDtoForUpdateToUser(UserDtoForUpdate userDtoForUpdate) {
+        User user = new User();
+        user.setId(userDtoForUpdate.id());
+        user.setEmail(userDtoForUpdate.email());
+        user.setPassword(userDtoForUpdate.password());
+        user.setNickName(userDtoForUpdate.nickName());
+        user.setRole(userDtoForUpdate.role());
+        user.setResult(userDtoForUpdate.result());
+        return user;
+    }
+
+    public UserDtoForResponse userDtoToUserDtoForResponse(UserDto userDto) {
+        return new UserDtoForResponse(
+                userDto.id(),
+                userDto.email(),
+                userDto.nickName(),
+                userDto.role()
+        );
+    }
+}
