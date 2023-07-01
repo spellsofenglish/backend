53b33fe59b6dd54d73e5f2e14003ae5fb4ce82ce
UladzislauM <marshal25@yandex.by>
2023-07-01 14:57:29 +0300

    Task for Refactor Registration: Change all JwtUtils

--- src/main/java/com/pat/soe/security/JwtUtils.java
+++ src/main/java/com/pat/soe/security/JwtUtils.java
@@ -1,72 +1,64 @@
 package com.pat.soe.security;
 
-import com.pat.soe.exception.UserValidationException;
-import com.pat.soe.service.user.UserAppDetails;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseCookie;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.WebUtils;
-
-import java.util.Date;
 
 @Component
 public class JwtUtils {
 
     public static final String INVALID_JWT = "Invalid JWT";
 
-    @Value("${app.jwtSecret}")
-    private String jwtSecret;
+    private final JwtDecoder decoder;
 
-    @Value("${app.jwtExpirationMs}")
-    private int jwtExpirationMs;
 
-    @Value("${app.jwtCookieName}")
-    private String jwtCookie;
+    @Value("${app.jwtName}")
+    private String jwtName;
+
+    @Autowired
+    public JwtUtils(JwtDecoder decoder) {
+        this.decoder = decoder;
+    }
+
+    public Cookie createCookieWithJwt(String token, int maxEge) {
+        Cookie cookie = new Cookie(jwtName, token);
+        cookie.setPath("/api");
+        cookie.setMaxAge(maxEge);
+        return cookie;
+    }
 
     public String getJwtFromCookies(HttpServletRequest request) {
-        Cookie cookie = WebUtils.getCookie(request, jwtCookie);
-        if (cookie != null) {
-            return cookie.getValue();
-        } else {
-            return null;
+        Cookie[] cookieArray = request.getCookies();
+        if (cookieArray != null) {
+            for (Cookie cookie : cookieArray) {
+                if (cookie.getName().equals(jwtName))
+                    return cookie.getValue();
+            }
         }
+        return null;
     }
 
-    public ResponseCookie generateJwtCookie(UserAppDetails userPrincipal) {
-        String jwt = generateTokenFromUsername(userPrincipal.getUsername());
-        return ResponseCookie.from(jwtCookie, jwt).path("/api").maxAge(24 * 60 * 60).httpOnly(true).build();
-    }
-
-    public ResponseCookie getCleanJwtCookie() {
-        return ResponseCookie.from(jwtCookie, null).path("/api").build();
+    public Cookie getCleanJwtCookie() {
+        Cookie cookie = new Cookie(jwtName, null);
+        cookie.setPath("/api");
+        cookie.setMaxAge(0);
+        return cookie;
     }
 
     public String getUserNameFromJwtToken(String token) {
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+        return decoder.decode(token).getClaims().get("sub").toString();
     }
 
     public boolean validateJwtToken(String authToken) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            decoder.decode(authToken);
             return true;
-        } catch (SignatureException e) {
-            new UserValidationException(INVALID_JWT);
+        } catch (JwtException e) {
+            throw new JwtException(INVALID_JWT);
         }
-
-        return false;
-    }
-
-    public String generateTokenFromUsername(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-                .compact();
     }
 }
