ffcefcd39202ab821642cfaa5c4f1c1ac3c71e6c
anton <antonkuchisnky86@gmail.com>
2023-07-08 10:55:09 +0300

    Adding data caching at service startup

--- game-service/src/main/java/ru/spellsofenglish/gameservice/service/TaskService.java
+++ game-service/src/main/java/ru/spellsofenglish/gameservice/service/TaskService.java
@@ -1,21 +1,28 @@
 package ru.spellsofenglish.gameservice.service;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 import ru.spellsofenglish.gameservice.models.Task;
 import ru.spellsofenglish.gameservice.repository.TaskRepository;
 
 
+import java.util.List;
 import java.util.Random;
 
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
+    private final CacheManager cacheManager;
 
     @Autowired
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, CacheManager cacheManager) {
         this.taskRepository = taskRepository;
+        this.cacheManager = cacheManager;
     }
+
     public Task getTask(){
         Random random=new Random();
         int randomNumber= random.nextInt(3600)+1;
@@ -27,6 +34,14 @@ public class TaskService {
             String audioExamplePath= "resources/" + task.getAudioExample();
         }
         return task;
+    }
+    @PostConstruct
+    public void loadAllDataToCache() {
+        Cache cache = cacheManager.getCache("tasks");
+        List<Task> tasks = taskRepository.findAll();
+        for (Task task : tasks) {
+            cache.put(task.getId(), task);
+        }
     }
 
 }
